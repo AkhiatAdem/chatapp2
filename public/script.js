dfaa6020ca8696b66c54e8bc262d990712bb5eff
adam akhiat <adamakhiat@adams-MacBook-Air.local>
2024-03-12 22:54:39 +0100

    initial commit

---
+++ public/script.js
@@ -0,0 +1,64 @@
+
+
+        
+const socket = io('http://localhost:5173');
+
+socket.on('connect', () => {
+    const login = document.querySelector(".login");
+const username = document.querySelector(".username");
+const log = document.querySelector(".log");
+const chat = document.querySelector(".chat");
+log.addEventListener("click",()=>{
+
+    login.remove();
+    chat.style.display = 'block';
+    socket.emit('x',`${username.value} is connected`);
+    
+});
+    
+
+    const hello = document.querySelector("#button");
+    const input = document.querySelector("#input");
+    hello.addEventListener("click", () => {
+        
+            socket.emit('h', input.value,username.value); 
+});
+
+socket.on('r',(message,username)=>{
+    displayMessage(message,username);
+})
+socket.on("xr",(message)=>{
+    const msgdiv = document.createElement("div");
+    document.querySelector(".chat").appendChild(msgdiv);
+    const msg = document.createElement("h4");
+    msg.innerText = message;
+    msgdiv.appendChild(msg);
+    const hr = document.createElement("hr");
+    msgdiv.appendChild(hr);
+
+})
+});
+
+function displayMessage(message,username){
+
+    const msgdiv = document.createElement("div");
+    document.querySelector(".chat").appendChild(msgdiv);
+
+    const msguser = document.createElement("h4");
+    msguser.innerText = `${username} at ${new Date().getHours()}:${new Date().getMinutes()}`;
+    msgdiv.appendChild(msguser);
+
+
+    const msg = document.createElement("h4");
+    msg.innerText = message;
+    msgdiv.appendChild(msg);
+    
+    const hr = document.createElement("hr");
+    msgdiv.appendChild(hr);
+
+
+}
+
+
+
+        
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+app.use(express.static('public'));
+
+const server = app.listen(5173, () => {
+    console.log('Server running!');
+});
+
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+
+
+    socket.on("h", (message,username) => {
+        
+        io.emit('r',message,username);
+    });
+    socket.on('x',(message)=>{
+        io.emit('xr',message);
+    })
+    
+});
+
